405fe6cc4e3476fd91844ddc48ec9d543b98e380
addoray1 <richardbampoe@gmail.com>
2024-01-21 15:16:52 -0800

    updated

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	char *content;
 	FILE *file;
 	size_t size = 0;
-	ssize_t read_line = 1;
+	size_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
+		read_line = getline(content, size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
